897dfe8e5824ff93a854374ffbf515f119deab48
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2025-12-08 14:07:59 +0100

    Update Customer and add new AddressModel

---
+++ CManager.Domain/AddresModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CManager.Domain
+{
+    public class AddresModel
+    {
+        public string StreetAdress { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string City { get; set; } = null!;
+
+    }
+
+}
--- CManager.Domain/Customer.cs
+++ CManager.Domain/Customer.cs
@@ -3,12 +3,13 @@
     public class Customer
 
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Telephone { get; set; } = string.Empty;
-        public string Address { get; set; } = string.Empty;
         public Guid Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Telephone { get; set; } = null!;
+        public AddresModel Address { get; set; }
 
     }
+
 }
