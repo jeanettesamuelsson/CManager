24f3980223bdc37a4e32a5d02646dfbc1729a0bc
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2025-12-09 15:00:42 +0100

    Add interface to customer repository

---
+++ CManager.Business/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CManager.Business.Interfaces
+{
+    public interface ICustomerRepository  //Add more methods - CRUD?
+
+
+    {
+        //method to get all customers
+        List<CustomerModel> GetAllCustomers();
+
+        //method to save customer to file
+        bool SaveCustomers(List<CustomerModel> customers);
+
+    }
+}
