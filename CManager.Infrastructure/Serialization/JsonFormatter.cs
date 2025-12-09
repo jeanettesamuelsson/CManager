3ef5dccab8d600c0e04fced2e58ee7a7f2c1bc2c
Jeanette Samuelsson <jeanette.e.samuelsson@outlook.com>
2025-12-09 14:30:55 +0100

    Add json formatter

---
+++ CManager.Infrastructure/Serialization/JsonFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace CManager.Infrastructure.Serialization
+{
+    public class JsonFormatter
+    {
+        //serialization options
+
+        private static readonly JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            PropertyNameCaseInsensitive = true,
+        };
+
+        //Serialize from C# object to JSON string
+        public static string Serialize<T>(T content) => JsonSerializer.Serialize(content, options);
+
+        //Deserialize from JSON string to C# object
+        public static T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json, options) ?? default;
+    }
+}
